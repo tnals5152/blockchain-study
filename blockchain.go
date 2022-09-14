7ce9da724fb46cb566d30cd11848ec8b276dbf45
tnals5152 <tnals5152@gmail.com>
2022-09-15 03:14:20 +0900

    test

---
+++ blockchain.go
@@ -0,0 +1,48 @@
+package block
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct { //block header
+	PrevBlockHash []byte //이전 블록의 해시값
+	Hash          []byte //해시값
+	Timestamp     int64  //시간
+	Data          []byte //데이터
+}
+
+type Blockchain struct { //블록들의 연결
+	blocks []*Block
+}
+
+func (b *Block) SetHash() {
+	header := bytes.Join([][]byte{
+		b.PrevBlockHash,
+		b.Data,
+		[]byte(strconv.FormatInt(b.Timestamp, 16)), //정수를 16진수로 변환
+	}, []byte{})
+	hash := sha256.Sum256(header)
+	b.Hash = hash[:]
+}
+
+//새 블록 생성
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{prevBlockHash, []byte{}, time.Now().Unix(), []byte(data)}
+	block.SetHash()
+
+	return block
+}
+
+func NewBlockchain() *Blockchain { //genesis block : 블록체인의 가장 첫 블록
+	return &Blockchain{[]*Block{
+		NewBlock("Genesis Block", []byte{}),
+	}}
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	block := NewBlock(data, bc.blocks[len(bc.blocks)-1].Hash)
+	bc.blocks = append(bc.blocks, block)
+}
--- blockchain1.go
+++ blockchain1.go
@@ -1,57 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"blockchain/block"
 	"fmt"
-	"strconv"
-	"time"
 )
 
 //프로토타입
 
-type Block struct { //block header
-	PrevBlockHash []byte //이전 블록의 해시값
-	Hash          []byte //해시값
-	Timestamp     int64  //시간
-	Data          []byte //데이터
-}
-
-type Blockchain struct { //블록들의 연결
-	blocks []*Block
-}
-
-func (b *Block) SetHash() {
-	header := bytes.Join([][]byte{
-		b.PrevBlockHash,
-		b.Data,
-		[]byte(strconv.FormatInt(b.Timestamp, 16)), //정수를 16진수로 변환
-	}, []byte{})
-	hash := sha256.Sum256(header)
-	b.Hash = hash[:]
-}
-
-//새 블록 생성
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{prevBlockHash, []byte{}, time.Now().Unix(), []byte(data)}
-	block.SetHash()
-
-	return block
-}
-
-func NewBlockchain() *Blockchain { //genesis block : 블록체인의 가장 첫 블록
-	return &Blockchain{[]*Block{
-		NewBlock("Genesis Block", []byte{}),
-	}}
-}
-
-func (bc *Blockchain) AddBlock(data string) {
-	block := NewBlock(data, bc.blocks[len(bc.blocks)-1].Hash)
-	bc.blocks = append(bc.blocks, block)
-}
-
 func main() {
-	bc := NewBlockchain() //새로운 블록체인 생성
+	bc := block.NewBlockchain() //새로운 블록체인 생성
 
 	//블록 추가
 	bc.AddBlock("Send 1")
